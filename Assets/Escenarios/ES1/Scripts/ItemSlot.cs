5268d98ab6886b5a91530a2e306d9d1eabb54cae
Ivan Contreras <a01540379@itesm.mx>
2020-09-18 15:46:19 -0500

    Subiendo version funcional del proyecto anterior

---
+++ Assets/Escenarios/ES1/Scripts/ItemSlot.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+// Clase para el espacio donde se deposita un objeto
+public class ItemSlot : MonoBehaviour, IDropHandler {
+
+    // Funcion donde identifica cuando se deja caer un objto en el espacio
+    public void OnDrop(PointerEventData eventData) {
+    	eventData.pointerDrag.GetComponent<DragDrop>().droppedOnSlot = true;
+        Debug.Log("OnDrop");
+        GameObject droppedObject = eventData.pointerDrag;
+        if (eventData.pointerDrag != null) {
+        	Vector3 position = GetComponent<RectTransform>().anchoredPosition;
+        	position.y += GlobalVariables.sumPos;
+            droppedObject.GetComponent<RectTransform>().anchoredPosition = position;
+            GlobalVariables.sumPos = GlobalVariables.sumPos - 120;
+        }
+    }
+}
