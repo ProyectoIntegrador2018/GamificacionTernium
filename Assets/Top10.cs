3b8694eaf12a2172f64b53282fc49ed784eab41e
napoleon lazo <napoleon.lazo@hotmail.com>
2020-09-23 00:32:58 -0500

    Display missions list

---
+++ Assets/Top10.cs
@@ -0,0 +1,46 @@
+﻿using Packages.Rider.Editor.UnitTesting;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Top10 : MonoBehaviour
+{
+
+    public TextAsset MissionNamesTextFile;
+    public GameObject missionPrefab;
+    private Missions missions;
+
+    private class Missions {
+
+        public string[] MissionNames;
+
+        Missions() {
+            MissionNames = new string[] { };
+        }
+    }
+
+    void instantiateMissionList() {
+        GameObject mission;
+
+        foreach (string missionName in missions.MissionNames) {
+            mission = Instantiate(missionPrefab);
+            mission.transform.SetParent(GameObject.Find("Mision Container").transform);
+            mission.transform.localScale = new Vector2((float)0.73, (float)1.006147);
+            mission.transform.GetChild(1).GetComponent<Text>().text = missionName;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        missions = JsonUtility.FromJson<Missions>(MissionNamesTextFile.text);
+        instantiateMissionList();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
