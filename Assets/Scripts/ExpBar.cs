c36e47d9c89ed206ccc49e819fa6fad6b35bc151
napoleon lazo <napoleon.lazo@hotmail.com>
2020-09-26 17:51:56 -0500

    Finished exp bar prototype

---
+++ Assets/Scripts/ExpBar.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[ExecuteInEditMode()]
+public class ExpBar : MonoBehaviour
+{
+
+    public int max;
+    public int min;
+    public int current;
+    public Image bar;
+    public Transform particles;
+    public Text expValues;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        getCurrentFill();
+    }
+
+    void getCurrentFill() {
+
+        RectTransform edge = particles.GetComponent<RectTransform>();
+
+        float currentOffset = current - min;
+        float maximumOffset = max - min;
+        float fillAmount = currentOffset / maximumOffset;
+        bar.fillAmount = fillAmount;
+
+        expValues.text = "Exp: " + current.ToString() + "/" + max.ToString();
+
+        edge.anchorMin = new Vector2(fillAmount, edge.anchorMin.y);
+        edge.anchorMax = new Vector2(fillAmount, edge.anchorMin.y);
+        edge.anchoredPosition = Vector2.zero;
+    }
+
+}
