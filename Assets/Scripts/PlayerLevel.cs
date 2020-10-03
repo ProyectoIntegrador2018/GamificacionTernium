cbf0c40b66c025f0f09cf4d7f502c1b29a284ffd
napoleon lazo <napoleon.lazo@hotmail.com>
2020-10-03 18:53:01 -0500

    Created Level up logic and animation

---
+++ Assets/Scripts/PlayerLevel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerLevel : MonoBehaviour
+{
+
+    private Text playerLevelTxt;
+    private int aux;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerInfoEventSystem.current.onExpBarFill += OnExpBarFill;
+        PlayerInfoEventSystem.current.onFinishExpGain += OnFinishExpGain;
+        playerLevelTxt = transform.GetChild(0).GetComponent<Text>();
+        //playerLevelTxt.text = Database.getNivelJugador().ToString();
+        //aux = Int32.Parse(playerLevelTxt.text);
+        aux = 1;
+    }
+
+    private void IncreaseLevel() {
+        aux++;
+        playerLevelTxt.text = aux.ToString();
+        LeanTween.scale(gameObject, new Vector3(1f, 1f, 0), 0.1f).setOnComplete(() => {
+            LeanTween.move(transform.parent.gameObject.GetComponent<RectTransform>(), new Vector3(0, 0, 0), 1f).setDelay(0.5f).setEase(LeanTweenType.easeInOutBack).setOnComplete(() => {
+                PlayerInfoEventSystem.current.FinishLevelUpAnimation();
+            });
+        });
+    }
+
+    private void OnExpBarFill() {
+        PlayerInfoEventSystem.current.StartLevelUpAnimation();
+        LeanTween.move(transform.parent.gameObject.GetComponent<RectTransform>(), new Vector3(109f, 435f, 0), 1f).setEase(LeanTweenType.easeInOutBack).setOnComplete(() => {
+            LeanTween.scale(gameObject, new Vector3(1.5f, 1.5f, 0), 0.1f).setOnComplete(IncreaseLevel);
+        });
+        
+    }
+
+    private void OnFinishExpGain() {
+        Database.setNivelJugador(aux);
+        Database.saveData();
+        WinCase.enableButtons();
+    }
+
+    private void OnDestroy() {
+        PlayerInfoEventSystem.current.onExpBarFill -= OnExpBarFill;
+        PlayerInfoEventSystem.current.onFinishExpGain -= OnFinishExpGain;
+    }
+}
