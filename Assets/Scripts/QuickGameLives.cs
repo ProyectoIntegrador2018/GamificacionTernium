8ab8b71d76aef1b0f578147cd99a8c67f558e7bc
napoleon lazo <napoleon.lazo@hotmail.com>
2020-10-24 18:19:32 -0500

    Initial setup for quick game lives system

---
+++ Assets/Scripts/QuickGameLives.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuickGameLives : MonoBehaviour
+{
+    int maxLives = 3;
+    int currentLives;
+
+    void setCurrentLivesSprites() {
+        for(int i = maxLives; i > currentLives; i--) {
+            this.transform.GetChild(i - 1).GetComponent<Image>().color = new Color(0.5f, 0.5f, 0.5f, 1f);
+        }
+    }
+
+    void regenerateLive() {
+        this.transform.GetChild(currentLives).GetComponent<Image>().color = new Color(1f, 1f, 1f, 1f);
+        currentLives++;
+    }
+
+    void OnTimeframeReached() {
+        regenerateLive();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentLives = 0;
+        setCurrentLivesSprites();
+        QuickGameEventSystem.current.onTimeframeReached += OnTimeframeReached;
+    }
+
+    private void OnDestroy() {
+        QuickGameEventSystem.current.onTimeframeReached -= OnTimeframeReached;
+    }
+}
---
+++ Assets/Scripts/QuickGameLivesTimer.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuickGameLivesTimer : MonoBehaviour
+{
+
+    Text timer;
+    float timerAmount = 15;
+    float timeRegenerateLive = 5;//1800;
+    float hours;
+    float minutes;
+    float seconds;
+    int lastWhole;
+    bool isRunning;
+
+    string formatTime(float time) {
+
+        string aux = time.ToString();
+
+        if (time < 10) {
+            aux = "0" + aux;
+        }
+
+        return aux;
+    }
+
+    void displayCurrentTime() {
+        hours = Mathf.FloorToInt(timerAmount / 3600);
+        minutes = Mathf.FloorToInt(timerAmount % 3600) / 60;
+        seconds = Mathf.FloorToInt(timerAmount % 60);
+        if (hours != 0) {
+            timer.text = formatTime(hours) + ":" + formatTime(minutes) + ":" + formatTime(seconds);
+        }
+        else {
+            timer.text = formatTime(minutes) + ":" + formatTime(seconds);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = GetComponent<Text>();
+        //timerAmount += 1;
+        if (timerAmount > 1) {
+            isRunning = true;
+        }
+        displayCurrentTime();
+    }
+
+    IEnumerator waitBeforeDisappearing() {
+        yield return new WaitForSeconds(0f);
+        this.transform.parent.gameObject.SetActive(false);
+    }
+
+    void canRegenerateLive() {
+        if (Mathf.FloorToInt(timerAmount) % timeRegenerateLive == 0 && lastWhole != Mathf.FloorToInt(timerAmount)) {
+            QuickGameEventSystem.current.OnTimeframeReached();
+            lastWhole = Mathf.FloorToInt(timerAmount);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isRunning) {
+            if (Mathf.FloorToInt(timerAmount) > 0) {
+                //print(timerAmount);
+                timerAmount -= Time.deltaTime;
+                canRegenerateLive();
+                displayCurrentTime();
+            }
+            else {
+                //StartCoroutine(waitBeforeDisappearing());
+                this.transform.parent.gameObject.SetActive(false);
+                isRunning = false;
+            }
+        }
+    }
+}
